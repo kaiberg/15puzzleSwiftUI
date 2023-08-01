2d28466ff7269347324e14a02ffe06d783c20161
Kai Delay <kbde@riseup.net>
2023-08-01 20:11:50 +0200

    Initial Commit

---
+++ 15puzzle/_5puzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5puzzleApp.swift
+//  15puzzle
+//
+//  Created by kai delay on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct _5puzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
