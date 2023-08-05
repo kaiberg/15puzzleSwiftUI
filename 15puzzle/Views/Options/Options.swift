e0a87e217bdc5ee857b3097711eca51508c5dcbc
Kai Delay <kbde@riseup.net>
2023-08-05 15:52:32 +0200

    added tab bar, refactored views into distinct groups

---
+++ 15puzzle/Views/Options/Options.swift
@@ -0,0 +1,18 @@
+//
+//  Options.swift
+//  15puzzle
+//
+//  Created by kai delay on 05/08/2023.
+//
+
+import SwiftUI
+
+struct Options: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Options()
+}
